ff9f378354442613634d60a1717856f3258eb729
tritren <metanon2@gmail.com>
2019-05-30 10:34:47 +0300

    изменил путь bootstrap.min.js и bootstrap.min.css/ структурировал теги

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    
+  <head>  
     <title>Eva's TinyLand</title>
     <?php require_once('C:/xampp/htdocs/tinyland/head.php'); ?>
-
   </head>
-
   <body>
     <h1>Hello, New version!</h1>
-
-    <script src="js/bootstrap.min.js/"></script>
+    <script src="bootstrap.min.js/"></script>
   </body>
 </html>
-
