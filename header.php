481f23ad466740dd07df2ed6e233b895acdbe082
tritren <metanon2@gmail.com>
2019-05-31 10:36:06 +0300

    что то подобное на header. добавил favicon

---
+++ header.php
@@ -0,0 +1,31 @@
+<header>
+	<div class="container-fluid" >
+		<section class="home-header">
+			<div class="container">
+				<div class="row">
+				<div class="col">
+					<a href="tel:+375297082921">
+						<i class="fa fa-phone"></i>
+						<span>+375(29)7082921</span>
+					</a>
+				</div>
+				<div class="col">
+					<div class="contacts-wrap_item">
+						<i class="fa fa-map-marker"></i>
+						<span>Ул. Шугаева, 17<br/>Метро Уручье</span>
+					</div>
+				</div>
+				<div class="col">
+					<i class="fa fa-clock-o" ></i>
+					<span>Пн - Сб: 9:30 - 20:00</span>
+				</div>
+				<div class="col">
+					<div class="contacts-wrap_item_social">
+						<a href="https://www.instagram.com/evastinyland/" target="_blank"><i class="fa fa-instagram"><img class="instagram"src="instagram.png"></i></a>
+					</div>
+				</div>
+			    </div>
+			</div>
+		</section>
+	</div>
+</header>
