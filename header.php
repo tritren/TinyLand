9723fc55e5c95be1ba853a4cf3b6c3067e6e91ff
tritren <metanon2@gmail.com>
2019-06-01 16:14:13 +0300

    готовый header

--- header.php
+++ header.php
@@ -1,31 +1,32 @@
-<header>
-	<div class="container-fluid" >
-		<section class="home-header">
-			<div class="container">
+		<header class="header" id="header">
+			<div class="container-fluid">
 				<div class="row">
-				<div class="col">
-					<a href="tel:+375297082921">
-						<i class="fa fa-phone"></i>
-						<span>+375(29)7082921</span>
-					</a>
-				</div>
-				<div class="col">
-					<div class="contacts-wrap_item">
-						<i class="fa fa-map-marker"></i>
-						<span>Ул. Шугаева, 17<br/>Метро Уручье</span>
+					<div class="col">
+						<div class="d-flex flex-wrap justify-content-between">
+							<div class="mx-auto">
+								<a href="tel:+375297082921">
+									<i class="fa fa-phone" aria-hidden="true"></i>
+									<span>+375(29)7082921</span>
+								</a></div>
+							<div class="mx-auto d-none d-sm-block">
+								<div class="contacts-wrap_item">
+									<i class="fa fa-map-marker" aria-hidden="true"></i>
+									<span>ул. Шугаева, 17</span>
+								</div>
+							</div>
+							<div class="mx-auto d-none d-sm-block">
+								<i class="fa fa-clock-o" aria-hidden="true"></i>
+								<span>Пн - Сб: 9:30 - 20:00</span>
+							</div>							
+							<div class="mx-auto">
+								<div class="contacts-wrap_item_social">
+									<a href="https://www.instagram.com/evastinyland/" target="_blank" data-toggle="tooltip" title="Актуальные новости!"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="col">
-					<i class="fa fa-clock-o" ></i>
-					<span>Пн - Сб: 9:30 - 20:00</span>
-				</div>
-				<div class="col">
-					<div class="contacts-wrap_item_social">
-						<a href="https://www.instagram.com/evastinyland/" target="_blank"><i class="fa fa-instagram"><img class="instagram"src="instagram.png"></i></a>
-					</div>
-				</div>
-			    </div>
 			</div>
-		</section>
-	</div>
-</header>
+		</header>
+
+
--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!doctype html>
 <html lang="ru">
   <head> 
-    <link rel="shortcut icon" href="favicon.png" type="image/png">
     <title>Eva's TinyLand</title>
     <meta name="description" content="Детский развивающий центр в Уручье, Раннее развитие с 1 до 3 лет, Английский язык с 3 до 12 лет, Английский язык с 2х лет - растим билингва, Английский для малышей, Подготовка к школе 4-6 лет, ИЗО-студия, Логопед">
     <meta name="keywords" content="Детский центр уручье, школа развития уручье, детский развивающий центр уручье, подготовка к школе уручье, английский язык для детей уручье, английский язык для малышей уручье, изо-студия уручье, развитие детей уручье, детский клуб уручье,центр развития ребенка уручье, развитие детей уручье,английский язык для самых маленьких уручье">
     <?php require_once $_SERVER['DOCUMENT_ROOT'].'/head.php';?>
   </head>
   <body>
-    <link rel="stylesheet" href="styles.css">
     <?php require_once $_SERVER['DOCUMENT_ROOT'].'/header.php';?>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'].'/navigation.php';?>
     <?php require_once $_SERVER['DOCUMENT_ROOT'].'/map.php';?>
   </body>
 </html>
