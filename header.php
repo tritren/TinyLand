49ecea1323cada9f5794b294b2fa7f68c9bdfec3
tritren <metanon2@gmail.com>
2019-06-03 14:45:15 +0300

    header.php заменили mx-auto col-sm...))

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 <section class="header">
 	<div class="container contact">
 		<div class="row">
-			<div class="phone_number mx-auto">
+			<div class="col-7 col-sm phone_number">
 				<a href="tel:+375297082921"><i class="fa fa-phone fa-lg" aria-hidden="true"></i><span>+375297082921</span></a>
 			</div>
-			<div class="location mx-auto d-none d-sm-block">
+			<div class="d-none d-sm-block col-sm location">
 				<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span> ул. Шугаева, 17<br>метро Уручье</span>
 			</div>
-			<div class="clock mx-auto d-none d-sm-block">
+			<div class="d-none d-sm-block col-smclock">
 				<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span>  Пн - Сб: 9:30 - 20:00</span>
 			</div>
-			<div class="social_network mx-auto">
+			<div class="col-5 col-sm social_network ">
 				<a href="https://www.instagram.com/evastinyland/" target="_blank" data-toggle="tooltip" title="Актуальные новости!"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i><span> Instagram</span></a>
 			</div>
 		</div>
