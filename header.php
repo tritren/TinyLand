19382f0911eda74b686a4ef2fc93b7a6da3238d4
tritren <metanon2@gmail.com>
2019-06-04 15:56:19 +0300

    make changes

--- header.php
+++ header.php
@@ -4,13 +4,13 @@
 			<div class="col-7 col-sm d-flex align-items-center phoneNumber">
 				<a href="tel:+375297082921"><i class="fa fa-phone fa-lg" aria-hidden="true"></i><span>+375297082921</span></a>
 			</div>
-			<div class="d-none col-sm location d-sm-flex align-items-center">
-				<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span> ул. Шугаева, 17<br>метро Уручье</span>
+			<div class="d-none col-sm location d-sm-flex align-items-center text-center text-md-left">
+				<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span>ул. Шугаева, 17<br>метро Уручье</span>
 			</div>
-			<div class="d-none col-md clok clock d-md-flex align-items-center">
-				<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span>  Пн - Сб: 9:30 - 20:00</span>
+			<div class="d-none clock d-lg-flex align-items-center text-center">
+				<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span>Пн - Сб: 9:30 - 20:00</span>
 			</div>
-			<div class="col-5 col-sm d-flex align-items-center justify-content-end socialNetwork">
+			<div class="col-5 col-sm d-flex align-items-center justify-content-end  socialNetwork">
 				<a href="https://www.instagram.com/evastinyland/" target="_blank"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i><span>Instagram</span></a>
 			</div>
 		</div>
--- navigation.php
+++ navigation.php
@@ -1,30 +1,27 @@
 <nav>
-	<div class="container">
-		<div class="row">
-			<div class="navbar navbar-expand-md navbar-light bg-light col-sm-12">
- 				 <a class="navbar-brand col-6" href="http://tinyland.by"><img src="img/eva.png"></a>
- 					 <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
-   					  <span class="navbar-toggler-icon"></span>
- 					 </button>
- 				 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-   					 <ul class="navbar-nav">
-    					  <li class="nav-item">
-    						    <a class="nav-link" href="http://tinylande.by">ГЛАВНАЯ<span class="sr-only"></span></a>
-    					  </li>
-    					  <li class="nav-item">
-     						   <a class="nav-link" href="#">ПРОГРАММЫ</a>
-    					  </li>
-    					  <li class="nav-item">
-       							 <a class="nav-link" href="#">РАСПИСАНИЕ</a>
-     					 </li>
-     					 <li class="nav-item">
-      						  <a class="nav-link" href="#">НОВОСТИ</a>
-     					 </li>
-   					 </ul>
-  				</div>
-			</div>
-		</div>
-	</div>
+   <div class="container">
+      <div class="row">
+         <div class="navbar navbar-expand-md navbar-light bg-light col-sm-12">
+            <a class="navbar-brand col-6" href="http://tinyland.by"><img src="img/eva.png"></a>
+            <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+               <ul class="navbar-nav">
+                  <li class="nav-item">
+                     <a class="nav-link" href="http://tinylande.by">ГЛАВНАЯ<span class="sr-only"></span></a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">ПРОГРАММЫ</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">РАСПИСАНИЕ</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">НОВОСТИ</a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
 </nav>
-
-
