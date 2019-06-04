2cae9094a8ce2e731cfba77b6c4b899719be9b1c
tritren <metanon2@gmail.com>
2019-06-05 00:17:03 +0300

    Navigation вроде бы нормально. только не смог сделать цвет такой же как и у ссылок на инст и телефон при  hover и active

--- head.php
+++ head.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="robots" content="all"/>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="js/main.js"type="text/javascript"></script>
@@ -8,3 +9,5 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
--- navigation.php
+++ navigation.php
@@ -2,22 +2,22 @@
    <div class="container">
       <div class="row">
          <div class="navbar navbar-expand-md navbar-light bg-light col-sm-12">
-            <a class="navbar-brand col-6" href="http://tinyland.by"><img src="img/eva.png"></a>
-            <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+            <a class="navbar-brand col" href="http://tinyland.by"><img src="img/eva.png"></a>
+            <button class="navbar-toggler btn btn-outline-light" type="button"  data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                <ul class="navbar-nav">
-                  <li class="nav-item">
-                     <a class="nav-link" href="http://tinylande.by">ГЛАВНАЯ<span class="sr-only"></span></a>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn  btn-lg active" role="button" aria-pressed="true" href="http://tinyland.by">ГЛАВНАЯ<span class="sr-only"></span></a>
                   </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="#">ПРОГРАММЫ</a>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn btn-lg active" role="button" aria-pressed="true" href="#">ПРОГРАММЫ</a>
                   </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="#">РАСПИСАНИЕ</a>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn btn-lg active" role="button" aria-pressed="true" href="#">РАСПИСАНИЕ</a>
                   </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="#">НОВОСТИ</a>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn btn-lg active" role="button" aria-pressed="true" href="#">НОВОСТИ</a>
                   </li>
                </ul>
             </div>
