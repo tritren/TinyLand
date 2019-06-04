3e71ec4945ec7189f406a9456513af4311a85ef8
tritren <metanon2@gmail.com>
2019-06-04 22:38:26 +0300

    header(version 19) your version is much more harmonious

--- header.php
+++ header.php
@@ -1,16 +1,16 @@
 <header>
 	<div class="container contact">
 		<div class="row">
-			<div class="col-7 col-sm d-flex align-items-center phoneNumber">
+			<div class="col-7 col-sm-4 col-lg-3 d-flex align-items-center phoneNumber">
 				<a href="tel:+375297082921"><i class="fa fa-phone fa-lg" aria-hidden="true"></i><span>+375297082921</span></a>
 			</div>
-			<div class="d-none col-sm location d-sm-flex align-items-center text-center text-md-left">
+			<div class="d-none col-sm-5 col-lg-3 location d-sm-flex align-items-center justify-content-center">
 				<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span>ул. Шугаева, 17<br>метро Уручье</span>
 			</div>
-			<div class="d-none clock d-lg-flex align-items-center text-center">
+			<div class="d-none col-lg-4 clock d-lg-flex align-items-center justify-content-center">
 				<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span>Пн - Сб: 9:30 - 20:00</span>
 			</div>
-			<div class="col-5 col-sm d-flex align-items-center justify-content-end  socialNetwork">
+			<div class="col-5 col-sm-3 col-lg-2 d-flex align-items-center justify-content-end  socialNetwork">
 				<a href="https://www.instagram.com/evastinyland/" target="_blank"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i><span>Instagram</span></a>
 			</div>
 		</div>
