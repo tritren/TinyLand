05991ab116736944b37ac2a5b748121506be9826
tritren <metanon2@gmail.com>
2019-06-05 12:31:35 +0300

    navigation

---
+++ navigation.php
@@ -0,0 +1,27 @@
+<nav>
+   <div class="container">
+      <div class="row">
+         <div class="navbar navbar-expand-md navbar-light bg-light col-sm-12">
+            <a class="navbar-brand col" href="http://tinyland.by"><img src="img/eva.png"></a>
+            <button class="navbar-toggler btn btn-outline-light" type="button"  data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+               <ul class="navbar-nav">
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn  btn-lg active" role="button" aria-pressed="true" href="http://tinyland.by">ГЛАВНАЯ<span class="sr-only"></span></a>
+                  </li>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn btn-lg active" role="button" aria-pressed="true" href="http://tinyland.by/programi/">ПРОГРАММЫ</a>
+                  </li>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn btn-lg active" role="button" aria-pressed="true" href="http://tinyland.by/raspisanie/">РАСПИСАНИЕ</a>
+                  </li>
+                  <li class="nav-item d-flex justify-content-center">
+                     <a class="nav-link font-weight-normal badge badge-light btn btn-lg active" role="button" aria-pressed="true" href="http://tinyland.by/novosti/">НОВОСТИ</a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </div>
+   </div>
+</nav>
