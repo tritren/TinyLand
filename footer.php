a8cb096ad4e3313db06bc4649a0a2266a8d016e7
tritren <metanon2@gmail.com>
2019-06-08 00:20:57 +0300

    foooter

--- footer.php
+++ footer.php
@@ -1,25 +1,25 @@
 <div class="footer">
-	<div class="container ">
+	<div class="container">
 		<div class="row ">
-			<div class="col-5">
-				<div class="footerPhoneNumber">
-					<a href="tel:+375297082921"><i class="fa fa-phone fa-lg" aria-hidden="true"></i><span>+375297082921</span></a>
+			<div class="col-4">
+				<div class="footerPhoneNumber d-flex align-items-center">
+					<a href="tel:+375297082921"><i class="fa fa-phone" aria-hidden="true"></i><span>+375297082921</span></a>
 				</div>
 				<div class="footerLocation">
-					<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span>ул. Шугаева, 17<br>метро Уручье</span>
+					<i class="fa fa-map-marker" aria-hidden="true"></i><span>ул. Шугаева, 17<br>метро Уручье</span>
 				</div>
 				<div class="footerClock">
-					<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span>Пн - Сб: 9:30 - 20:00</span>
+					<i class="fa fa-clock-o" aria-hidden="true"></i><span>Пн - Сб: 9:30 - 20:00</span>
 				</div>
 			</div>
-			<div class="col-3 footerSocialNetwork d-flex  justify-content-center ">
-				<a href="https://www.instagram.com/evastinyland/" target="_blank"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i><span></span></a>
+			<div class="col-4 footerSocialNetwork d-flex  justify-content-center">
+				<a href="https://www.instagram.com/evastinyland/" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i><span></span></a>
 			</div>
-			<div class="col-3 footerLogo">
+			<div class="col-4 footerLogo d-flex align-items-center justify-content-end ">
 				<a href="http://tinyland.by"><img src="img/footerlogo.svg"></a>
 			</div>
-			<div class="col-12 footerFriends">
-				<h3>Наши друзья:</h3>
+			<div class="col footerFriends">
+				<h2>НАШИ ДРУЗЬ:</h2>
 				<a href="https://klenik.by/"><img src="img/partner.jpg"></a>
 			</div>
 		</div>
