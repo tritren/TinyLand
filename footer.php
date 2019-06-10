75c5f392f90703a90aacfd160e2be8563b154ad1
tritren <metanon2@gmail.com>
2019-06-11 00:34:34 +0300

    исправил footer

--- footer.php
+++ footer.php
@@ -1,27 +1,27 @@
-<div class="footer">
+<footer>
 	<div class="container">
 		<div class="row ">
-			<div class="col-6 col-md-4 d-none d-sm-block contactDetails">
-				<div class="footerPhoneNumber align-items-center">
+			<div class="col-8 col-md-4 contactDetails">
+				<div class="footerPhoneNumber d-none d-sm-flex align-items-center">
 					<a href="tel:+375297082921"><i class="fa fa-phone fa-lg" aria-hidden="true"></i><span>+375297082921</span></a>
 				</div>
 				<div class="footerLocation d-flex align-items-center">
 					<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span> ул. Шугаева, 17<br> метро Уручье</span>
 				</div>
-				<div class="footerClock d-flex align-items-center">
+				<div class="footerClock d-none d-sm-flex align-items-center">
 					<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span> Пн - Сб: 9:30 - 20:00</span>
 				</div>
 				<div class="footerFriends d-none d-xl-flex align-items-center">
-				<span>НАШИ ДРУЗЬЯ:</span>
-				<a href="https://klenik.by/"><img src="img/partner.jpg"></a>
+					<span>НАШИ ДРУЗЬЯ:</span>
+					<a href="https://klenik.by/"><img src="img/partner.jpg"></a>
+				</div>
 			</div>
+			<div class="col-4 col-md-4  d-flex align-items-center  justify-content-center footerSocialNetwork">
+				<a href="https://www.instagram.com/evastinyland/" target="_blank"><span>Instagram</span><br><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
 			</div>
-			<div class="col col-md-4  d-flex align-items-center  justify-content-center footerSocialNetwork">
-				<a href="https://www.instagram.com/evastinyland/" target="_blank"><span>Instagram</span><br><i class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
-			</div>
-			<div class="col-4 d-none col-md-4  d-md-flex align-items-center justify-content-end footerLogo">
+			<div class="d-none col-md-4  d-md-flex align-items-center justify-content-end footerLogo">
 				<a href="http://tinyland.by"><img src="img/footerlogo.svg"></a>
 			</div>
 		</div>
 	</div>
-</div>
+</footer>
