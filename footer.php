1fd75a48b1308f555bb675625193b1ba6f5022bf
tritren <metanon2@gmail.com>
2019-06-11 16:17:38 +0300

    испраивл паддинг в футере, вертикал выр на мобилках, и цвет бекграунд

--- footer.php
+++ footer.php
@@ -2,22 +2,22 @@
 	<div class="container">
 		<div class="row ">
 			<div class="col-8 col-md-4 contactDetails">
-				<div class="footerPhoneNumber d-none d-sm-flex align-items-center">
+				<div class="footerPhoneNumber d-none d-md-block">
 					<a href="tel:+375297082921"><i class="fa fa-phone fa-lg" aria-hidden="true"></i><span>+375297082921</span></a>
 				</div>
-				<div class="footerLocation d-flex align-items-center">
+				<div class="footerLocation">
 					<i class="fa fa-map-marker fa-lg" aria-hidden="true"></i><span> ул. Шугаева, 17<br> метро Уручье</span>
 				</div>
-				<div class="footerClock d-none d-sm-flex align-items-center">
+				<div class="footerClock d-none d-md-block">
 					<i class="fa fa-clock-o fa-lg" aria-hidden="true"></i><span> Пн - Сб: 9:30 - 20:00</span>
 				</div>
-				<div class="footerFriends d-none d-xl-flex align-items-center">
+				<div class="footerFriends d-none d-xl-block ">
 					<span>НАШИ ДРУЗЬЯ:</span>
 					<a href="https://klenik.by/"><img src="img/partner.jpg"></a>
 				</div>
 			</div>
 			<div class="col-4 col-md-4  d-flex align-items-center  justify-content-center footerSocialNetwork">
-				<a href="https://www.instagram.com/evastinyland/" target="_blank"><span>Instagram</span><br><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+				<a href="https://www.instagram.com/evastinyland/" target="_blank"><span>Instagram</span><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
 			</div>
 			<div class="d-none col-md-4  d-md-flex align-items-center justify-content-end footerLogo">
 				<a href="http://tinyland.by"><img src="img/footerlogo.svg"></a>
