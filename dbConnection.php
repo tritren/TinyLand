107be13905c6b68588d59fa3928f7126080228ea
tritren <metanon2@gmail.com>
2019-06-19 00:10:42 +0300

    будем считать что научили меня

--- dbConnection.php
+++ dbConnection.php
@@ -1,15 +1,24 @@
 <?php
-function printResult($result_set) {
-	while (($row = $result_set->fetch_assoc() ) !=false) {
-		print_r ($row);
-		echo "<br />";	
-	}
-	echo "Количество записей" .$result_set->num_rowe."<br />----------------------";
-
-}
-$mysqli = new mysqli ("localhost:8080", "root", "", "tinyland");
+$mysqli = new mysqli ("localhost", "root", "", "tinyland");
 $mysqli->query ("SET NAMES 'utf8'");
-$$result_set = $mysqli->query ("SELECT * FROM `teachers`");
-printResult ($result_set);
-$mysqli->close();
+
+
+	function getTeachers() {
+		global $mysqli;
+		$result_set = $mysqli->query("SELECT * FROM `teachers`");
+
+		while (($row = $result_set->fetch_assoc() ) !=false) {
+			echo '<div class="col-md-4 teacherContainer">
+             	 <div class="teacherInfo d-flex align-items-center">
+              	  <img src="img/'.$row['Image'].'"  class="img-fluid">
+               	 <div class="teacherDescription">
+                	  <a href="teacher.php?id='.$row['Id'].'"><h4>'.$row['Name'].'</h4></a>
+                	  <h2>'.$row['Position'].'</h2>
+               	   <p class="d-none d-md-block">'.$row['Description'].'</p>
+               	 </div>
+             	 </div>
+          	  </div>';
+		}
+	$mysqli->close();
+	}
 ?>
