274e1e82d83dc4f5944d7e7a4db4f93c5c41c1e6
tritren <metanon2@gmail.com>
2019-06-19 18:23:57 +0300

    mobile first, style.css исправил для Programs

--- programs.php
+++ programs.php
@@ -2,37 +2,14 @@
 	<div class="container">
 		<div class="row">
 			<div class="programsTitleBlock col-12 text-center">
-              <h1>ПРОГРАММЫ</h1>
+				<h1>ПРОГРАММЫ</h1>
             </div>
             <div class="col-6 col-sm-4 col-md-3 programsContainer">
              	<div class="programsInfo d-flex align-items-center">
               	    <a href="teachers.php?id='.$row['Id'].'"><img src="img/excellentStudent.jpg"></a>
               	    <div class="programsDescription">
-              	    <p>Мини детский сад "Пчелки"</p><a href="teachers.php?id='.$row['Id'].'"><h4>Подробнее</h4></a>
-             		</div>
-          		</div>
-			</div>
-			 <div class="col-6 col-sm-4 col-md-3 programsContainer">
-             	<div class="programsInfo d-flex align-items-center">
-              	    <a href="teachers.php?id='.$row['Id'].'"><img src="img/excellentStudent.jpg"></a>
-              	    <div class="programsDescription">
-              	    <p>Мини детский сад "Пчелки"</p><a href="teachers.php?id='.$row['Id'].'"><h4>Подробнее</h4></a>
-             		</div>
-          		</div>
-			</div>
-			<div class="col-6 col-sm-4 col-md-3 programsContainer">
-             	<div class="programsInfo d-flex align-items-center">
-              	    <a href="teachers.php?id='.$row['Id'].'"><img src="img/excellentStudent.jpg"></a>
-              	    <div class="programsDescription">
-              	    <p>Мини детский сад "Пчелки"</p><a href="teachers.php?id='.$row['Id'].'"><h4>Подробнее</h4></a>
-             		</div>
-          		</div>
-			</div>
-			<div class="col-6 col-sm-4 col-md-3 programsContainer">
-             	<div class="programsInfo d-flex align-items-center">
-              	    <a href="teachers.php?id='.$row['Id'].'"><img src="img/excellentStudent.jpg"></a>
-              	    <div class="programsDescription">
-              	    <p>Мини детский сад "Пчелки"</p><a href="teachers.php?id='.$row['Id'].'"><h4>Подробнее</h4></a>
+              	    	<p>Мини детский сад "Пчелки"</p>
+              	    	<a href="teachers.php?id='.$row['Id'].'"><h4>Подробнее</h4></a>
              		</div>
           		</div>
 			</div>
