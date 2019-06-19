d91af07e6e650eb200ea32e880e473c8d8d5790c
tritren <metanon2@gmail.com>
2019-06-19 23:12:21 +0300

    подключил таблицу programs

--- dbConnection.php
+++ dbConnection.php
@@ -19,6 +19,23 @@ $mysqli->query ("SET NAMES 'utf8'");
              	 </div>
           	  </div>';
 		}
-	$mysqli->close();
-	}
+}
+
+  function getPrograms() {
+    global $mysqli;
+    $result_set = $mysqli->query("SELECT * FROM `programs`");
+
+    while (($row = $result_set->fetch_assoc() ) !=false) {
+      echo '<div class="col-6 col-sm-4 col-md-3 programsContainer">
+              <div class="programsInfo d-flex align-items-center">
+                <a href="teachers.php?id='.$row['Id'].'"><img src="img/'.$row['Image'].'"></a>
+                <div class="programsDescription">
+                 <p>'.$row['Summary'].'</p>
+                  <a href="teachers.php?id='.$row['Id'].'"><h4>Подробнее</h4></a>
+                </div>
+              </div>
+            </div>';
+    }
+    $mysqli->close();
+}
 ?>
---
+++ programs.php
@@ -0,0 +1,10 @@
+<section class="programs">
+	<div class="container">
+		<div class="row">
+			<div class="programsTitleBlock col-12 text-center">
+				<h1>ПРОГРАММЫ</h1>
+      </div>
+      <?php getPrograms();?>
+		</div>
+	</div>
+</section>
